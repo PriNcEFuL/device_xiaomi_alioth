a3f2c7778e7409a62262dbd26f864797f0bd541b
PriNcEFuL <amir123prince123@gmail.com>
2023-09-21 01:49:11 +0300

    Bring up Evo X

--- evolution_alioth.mk
+++ evolution_alioth.mk
@@ -7,28 +7,27 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-# GhostIcon
-$(call inherit-product-if-exists, vendor/ghosticon/ghosticon.mk)
-$(call inherit-product-if-exists, vendor/wallx/wallxayra.mk)
+# Inherit some common Evolution stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_USES_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_ENABLE_BLUR := true
+TARGET_USES_MIUI_CAMERA	:= true
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := lineage_alioth
+PRODUCT_NAME := evolution_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F3
 
-TARGET_SUPPORTS_QUICK_TAP := true
-
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_USES_BLUR := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_ENABLE_BLUR := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
